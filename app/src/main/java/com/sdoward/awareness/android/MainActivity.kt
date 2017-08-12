48353ddc24388e85fb68a8ef7edaa40f5a47bb2b
sdoward <sam.doward@gmail.com>
2017-08-12 14:40:45 +0200

    Remove fab

--- app/src/main/java/com/sdoward/awareness/android/MainActivity.kt
+++ app/src/main/java/com/sdoward/awareness/android/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.sdoward.awareness.android
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.awareness.Awareness
 import com.google.android.gms.common.api.GoogleApiClient
 import kotlinx.android.synthetic.main.main_activity.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,14 +14,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.main_activity)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener {
-            Snackbar.make(it, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
-        }
-
         val client = GoogleApiClient.Builder(this)
                 .addApi(Awareness.API)
                 .build()
         client.connect()
+
     }
 
 }
