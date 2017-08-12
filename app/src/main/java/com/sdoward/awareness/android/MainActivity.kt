32f077e95d685f2e82498153585807fa1ea10266
sdoward <sam.doward@gmail.com>
2017-08-12 14:28:16 +0200

    Configure Awareness Api

--- app/src/main/java/com/sdoward/awareness/android/MainActivity.kt
+++ app/src/main/java/com/sdoward/awareness/android/MainActivity.kt
@@ -3,6 +3,8 @@ package com.sdoward.awareness.android
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.awareness.Awareness
+import com.google.android.gms.common.api.GoogleApiClient
 import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener {
             Snackbar.make(it, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show()
         }
+
+        val client = GoogleApiClient.Builder(this)
+                .addApi(Awareness.API)
+                .build()
+        client.connect()
     }
-    
+
 }
