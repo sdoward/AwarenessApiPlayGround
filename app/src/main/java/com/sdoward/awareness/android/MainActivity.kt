8e6d6078a9f11d9227bc3ecd75b4d84e2bd6f70c
sdoward <sam.doward@gmail.com>
2017-08-13 10:00:15 +0200

    Improve lifecycle

--- app/src/main/java/com/sdoward/awareness/android/MainActivity.kt
+++ app/src/main/java/com/sdoward/awareness/android/MainActivity.kt
@@ -18,6 +18,12 @@ class MainActivity : AppCompatActivity() {
     val repository: Repository by lazy {
         Repository(UserManager(PreferenceManager.getDefaultSharedPreferences(this)).getIdentifier())
     }
+
+    val client: GoogleApiClient by lazy {
+        GoogleApiClient.Builder(this)
+                .addApi(Awareness.API)
+                .build()
+    }
     val disposable = CompositeDisposable()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,28 +32,27 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 0)
-
-        val client = GoogleApiClient.Builder(this)
-                .addApi(Awareness.API)
-                .build()
         client.connect()
         activitiesButton.setOnClickListener {
             Awareness.SnapshotApi.getActivityObservable(client)
                     .map { AwarenessModel(it) }
                     .doOnSuccess { repository.setData(it) }
                     .subscribe { activities -> infoTextView.text = activities.toString() }
+                    .addTo(disposable)
         }
         locationButton.setOnClickListener {
             Awareness.SnapshotApi.getLocationObservable(client)
                     .map { AwarenessModel(location = it) }
                     .doOnSuccess { repository.setData(it) }
                     .subscribe { location -> infoTextView.text = location.toString() }
+                    .addTo(disposable)
         }
         placesButton.setOnClickListener {
             Awareness.SnapshotApi.getPlacesObservable(client)
                     .map { AwarenessModel(places = it) }
                     .doOnSuccess { repository.setData(it) }
                     .subscribe { places -> infoTextView.text = places.toString() }
+                    .addTo(disposable)
         }
 
         getAllButton.setOnClickListener {
@@ -58,9 +63,14 @@ class MainActivity : AppCompatActivity() {
                     Function3<List<Activity>, Location, List<Place>, AwarenessModel> { activities, location, places -> AwarenessModel(activities, location, places) })
                     .doOnSuccess { repository.setData(it) }
                     .subscribe { awarenessModel -> infoTextView.text = awarenessModel.toString() }
-
+                    .addTo(disposable)
         }
 
     }
 
+    override fun onDestroy() {
+        client.disconnect()
+        disposable.clear()
+        super.onDestroy()
+    }
 }
---
+++ app/src/main/java/com/sdoward/awareness/android/RxExentions.kt
@@ -0,0 +1,8 @@
+package com.sdoward.awareness.android
+
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+fun Disposable.addTo(compositeDisposable: CompositeDisposable) {
+    compositeDisposable.add(this)
+}
